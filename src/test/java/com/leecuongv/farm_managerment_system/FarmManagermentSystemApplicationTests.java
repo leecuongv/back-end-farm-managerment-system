fa8a2a3143325712643ec82ada6505f1422bb460
Le Van Cuong <vancuongle65@gmail.com>
2025-10-25 21:58:06 +0700

    first commit

---
+++ src/test/java/com/leecuongv/farm_managerment_system/FarmManagermentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leecuongv.farm_managerment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmManagermentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
